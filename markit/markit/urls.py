41497769f2fc87684b791b7f91c23d41116bfc0e
Sepehr Hashemi <sepehr.hashemih@gmail.com>
2019-10-21 20:30:59 +0330

    Clean solution and fix names.

---
+++ markit/markit/urls.py
@@ -0,0 +1,11 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls.static import static
+from django.conf import settings
+
+urlpatterns  = [
+  path('admin/', admin.site.urls),
+  path('api/', include('users.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
