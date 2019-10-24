430132d3f660cb88d333a69b66ce2cc68de59ffd
Mohammad Mahdi Abdollahpour <mohammad.m.a.77@outlook.com>
2019-10-24 15:34:23 +0330

    Define routing paths.

--- Markit-Frontend/src/app/app-routing.module.ts
+++ Markit-Frontend/src/app/app-routing.module.ts
@@ -1,8 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: SignupComponent },
+  // { path: 'no-access', component: }
+
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
