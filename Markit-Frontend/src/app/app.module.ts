01b0fbcfd9b7d21b90f4123f20a829c1f73bfc54
Mohammad Mahdi Abdollahpour <mohammad.m.a.77@outlook.com>
2019-10-24 15:44:31 +0330

    Import necessary modules into app.module file.

--- Markit-Frontend/src/app/app.module.ts
+++ Markit-Frontend/src/app/app.module.ts
@@ -7,20 +7,32 @@ import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { ReactiveFormsModule } from '@angular/forms';
 import { MdComponentsModule } from './md-components/md-components.module';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { SignupComponent } from './signup/signup.component';
+import { HomeComponent } from './home/home.component';
+import {MatCardModule, MatListModule, MatProgressSpinnerModule, MatTabsModule, MatToolbarModule} from '@angular/material';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    SignupComponent
+    SignupComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     ReactiveFormsModule,
-    MdComponentsModule
+    MdComponentsModule,
+    MatToolbarModule,
+    MatTabsModule,
+    HttpClientModule,
+    MatListModule,
+    MatCardModule,
+    MatSnackBarModule,
+    MatProgressSpinnerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
