677a6ef59d5d44014156d802a120c80ab254a63e
Parisa Yalsavar <p.yalsavar@outlook.com>
2019-10-24 14:07:32 +0000

    Deleted markit from master

--- markit/calendars/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from calendars.models import *
-
-admin.site.register(Calendar)
-admin.site.register(Post)
--- markit/users/views.py
+++
@@ -1,7 +0,0 @@
-from allauth.socialaccount.providers.twitter.views import TwitterOAuthAdapter
-from rest_auth.registration.views import SocialConnectView
-from rest_auth.social_serializers import TwitterConnectSerializer
-
-class TwitterConnect(SocialConnectView):
-    serializer_class = TwitterConnectSerializer
-    adapter_class = TwitterOAuthAdapter
