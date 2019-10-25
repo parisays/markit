4eafad1cd22cc8413943ce6c54309ba866288be4
Mohammad Mahdi Abdollahpour <mohammad.m.a.77@outlook.com>
2019-10-25 07:24:14 +0330

    Implement logic of login component and update few changes in design according to it.

--- Markit-Frontend/src/app/login/login.component.ts
+++ Markit-Frontend/src/app/login/login.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators} from '@angular/forms'
+import {FormGroup, Validators, FormBuilder} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AuthenticationService } from '@services';
+import { first } from 'rxjs/operators';
+import {MatSnackBar} from '@angular/material';
 
 @Component({
   selector: 'app-login',
@@ -7,27 +11,58 @@ import { FormGroup, FormControl, Validators} from '@angular/forms'
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-  form = new FormGroup({
-    'email': new FormControl('', [
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [
       Validators.email,
       Validators.required
-    ]),
-    password: new FormControl('', Validators.required)
-});
+    ]],
+    password: ['', Validators.required]
+  });
+  loading = false; // TODO clean up
+  submitted = false;
+  returnUrl: string;
+  error = '';
 
-get email(){
-  return this.form.get('email');
-}
+  hide = true;
 
-get password(){
-  return this.form.get('password')
-}
+  constructor(
+    private fb: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authService: AuthenticationService,
+    private snackBar: MatSnackBar,
+  ) {
+    if (this.authService.currentUserValue) {
+      this.router.navigate(['/']);
+    }
+  }
 
-hide = true;
+  get f() { return this.loginForm.controls; }
 
-constructor() { }
+  ngOnInit() {
+    this.returnUrl = this.route.snapshot.queryParams.returnUrl || '/';
+  }
 
-ngOnInit() {
-}
+  onSubmit() {
+    this.submitted = true;
 
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authService.login(this.f.email.value, this.f.password.value)
+      .pipe(
+        first()
+      ).subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        err => {
+          this.error = err;
+          this.loading = false;
+          this.snackBar.open('Login failed!', 'Dismiss', { duration: 5000 });
+        }
+      );
+  }
 }
