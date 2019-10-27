8b0cfe718adf2fb24225d8b9d29e896d66ec749f
Mahnaz HaghaniNejad <mahnaz.hagh@outlook.com>
2019-10-27 16:06:57 +0330

    Add new post and twitter connect button

--- Markit-Frontend/src/app/post-list/post-list.component.ts
+++ Markit-Frontend/src/app/post-list/post-list.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import {animate, state, style, transition, trigger} from '@angular/animations';
+import { PostService } from '@services';
+import { Post } from '@models';
 
 @Component({
   selector: 'app-post-list',
@@ -15,41 +17,33 @@ import {animate, state, style, transition, trigger} from '@angular/animations';
 })
 export class PostListComponent implements OnInit {
 
-  dataSource = ELEMENT_DATA;
-  columnsToDisplay = ['title', 'date', 'time'];
+  isTwitterConnected = false;
+
+  dataSource = ELEMENT_DATA; //data source is posts
+  columnsToDisplay = ['title'];
   expandedElement: Post | null;
-  constructor() { }
+  constructor(private service: PostService) { }
 
   ngOnInit() {
+    this.service.getPosts().subscribe(response => {
+      // this.calendars = response.json();//todo
+    });
   }
 
-}
-
-export interface Post {
-  title: string;
-  date: string;
-  time: string;
-  content: string;
 }
 
 const ELEMENT_DATA: Post[] = [
   {
     title: 'Hydrogen',
-    time: '12:23:24',
-    date: '1/9/1999',
     content: `Hydrogen is a chemical element with symbol H and atomic number 1. With a standard
         atomic weight of 1.008, hydrogen is the lightest element on the periodic table.`
   }, {
     title: 'Helium',
-    time: '2:3:32',
-    date: '2/4/1987',
     content: `Helium is a chemical element with symbol He and atomic number 2. It is a
         colorless, odorless, tasteless, non-toxic, inert, monatomic gas, the first in the noble gas
         group in the periodic table. Its boiling point is the lowest among all the elements.`
   }, {
     title: 'Lithium',
-    time: '12:23:24',
-    date: '12/23/2009',
     content: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
         silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
         lightest solid element.`
