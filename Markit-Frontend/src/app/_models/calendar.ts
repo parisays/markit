820c9db31f2865bd8bde4014cab05f4308167299
YegMor <yegmor@gmail.com>
2019-10-27 20:56:57 +0330

    Sync UI and Logic of creating calendars

--- Markit-Frontend/src/app/_models/calendar.ts
+++ Markit-Frontend/src/app/_models/calendar.ts
@@ -1,7 +1,11 @@
 export class Calendar {
   id: number;
   title: string;
-  description?: string;
+  // description?: string;
   // user: User
   // posts?: Post[]
+
+  constructor(title: string) {
+    this.title = title;
+  }
 }
