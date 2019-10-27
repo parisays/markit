fa55f55528c9d709107c8d85fc79ab8992573700
Mohammad Mahdi Abdollahpour <mohammad.m.a.77@outlook.com>
2019-10-28 00:16:21 +0330

    Add twitter-auth route

--- Markit-Frontend/src/app/app-routing.module.ts
+++ Markit-Frontend/src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SignupComponent } from './signup/signup.component';
 import { HomeComponent } from './home/home.component';
+import {TwitterAuthComponent} from '@app/twitter-auth/twitter-auth.component';
 
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: SignupComponent },
+  { path: 'twitter-auth', component: TwitterAuthComponent },
   // { path: 'no-access', component: }
 
   { path: '**', redirectTo: '' }
