952a4f2c4174a2b0a9cd688b0bef9dd5b89f3541
Mohammad Mahdi Abdollahpour <mohammad.m.a.77@outlook.com>
2019-10-28 14:39:06 +0330

    Clean up header component & move twitter auth logic to posts component

--- Markit-Frontend/src/app/header/header.component.ts
+++ Markit-Frontend/src/app/header/header.component.ts
@@ -13,7 +13,6 @@ import {Router} from '@angular/router';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
-  private twitterAppData: { client_id: string, secret: string };
 
   constructor(
     private authService: AuthenticationService,
@@ -27,44 +26,5 @@ export class HeaderComponent implements OnInit {
   // login() {
   //   this.ro
   // }
-  onTwitterAuth() {
-    this.http.get<any>(`${environment.apiUrl}/api/v1.0/auth/twitter/`, {
-      headers: new HttpHeaders({
-        Authorization: `Token ${this.authService.currentUserValue.key}`
-      })
-    }).pipe(
-      map(data => {
-        if (data && data.provider && data.provider === 'twitter') {
-          return {
-            client_id: data.client_id as string,
-            secret: data.secret as string
-          };
-        } else {
-          return throwError(new Error('twitter oauth failed'));
-        }
-      })
-    ).subscribe(data => {
-        this.twitterAppData = data as { client_id: string, secret: string };
-        console.log(data);
-        this.http.get<any>(`${environment.apiUrl}/api/v1.0/auth/twitter/oauth`, {
-          headers: {
-            Authorization: `Token ${this.authService.currentUserValue.key}`
-          }
-        }).pipe(map(authData => {
-          if (authData && authData.url) {
-            return authData.url as string;
-          } else {
-            return throwError(new Error('twitter fetch token failed'));
-          }
-        })).subscribe(twitterUrl => {
-          console.log(twitterUrl);
-          window.location.href = twitterUrl as string;
-        }, error => {
-          console.log('twitter routing failed');
-        });
-      },
-      error => {
-        console.log(error);
-      });
-  }
+
 }
