57855a5cbafd8c568b94944584ac05b8c6d4e094
YegMor <yegmor@gmail.com>
2019-10-28 11:00:40 +0330

    Modify requests to http server

--- Markit-Frontend/src/app/_services/post.service.ts
+++ Markit-Frontend/src/app/_services/post.service.ts
@@ -1,23 +1,33 @@
-import { Injectable } from '@angular/core';
-import {HttpClient} from '@angular/common/http';
+import {Injectable} from '@angular/core';
+import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 import {environment} from '@environments/environment';
 import {Post} from '@models';
+import {AuthenticationService} from '@app/_services/auth.service';
+
 @Injectable({
   providedIn: 'root'
 })
 export class PostService {
-  private createPostEndpoint = `${environment.apiUrl}/calendar/post/`;
-  private listPostsEndpoint = `${environment.apiUrl}/calendar/post/?calendar_id=/`;
+  private createPostEndpoint = `${environment.apiUrl}/api/v1.0/calendar/post/`;
+  private listPostsEndpoint = `${environment.apiUrl}/api/v1.0/calendar/post/?calendar_id=/`;
 
-  constructor(private http: HttpClient) {
+  constructor(private http: HttpClient, private  authService: AuthenticationService) {
   }
 
   getPosts(calendarId: number) {
-    return this.http.get<Post[]>(this.listPostsEndpoint + calendarId.toString());//todo give calendar id in the url
+    return this.http.get<Post[]>(this.listPostsEndpoint + calendarId.toString(), {
+      headers: new HttpHeaders({
+        Authorization: `Token ${this.authService.currentUserValue.key}`
+      })
+    });// todo give calendar id in the url
   }
 
   createPost(post) {
-    return this.http.post(this.createPostEndpoint, JSON.stringify((post)));
+    return this.http.post(this.createPostEndpoint, JSON.stringify((post)), {
+      headers: new HttpHeaders({
+        Authorization: `Token ${this.authService.currentUserValue.key}`
+      })
+    });
   }
 }
