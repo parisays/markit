2aa2fadf47bea13f84aeee27131ad740647086f7
Mahnaz HaghaniNejad <mahnaz.hagh@outlook.com>
2019-11-06 14:36:17 +0330

    Design calendar wizard

---
+++ Markit-Frontend/src/app/calendar-wizard/calendar-wizard.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calendar-wizard',
+  templateUrl: './calendar-wizard.component.html',
+  styleUrls: ['./calendar-wizard.component.scss']
+})
+export class CalendarWizardComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
