355bff9a9b9e21d3735195b0a5c65b4a0cc63a13
Mohammad Mahdi Abdollahpour <mohammad.m.a.77@outlook.com>
2019-11-07 15:02:19 +0330

    Remove redundant tokens from other services

--- Markit-Frontend/src/app/_services/post.service.ts
+++ Markit-Frontend/src/app/_services/post.service.ts
@@ -3,7 +3,6 @@ import {HttpClient, HttpHeaders} from '@angular/common/http';
 
 import {environment} from '@environments/environment';
 import {Post} from '@models';
-import {AuthenticationService} from '@app/_services/auth.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,22 +11,14 @@ export class PostService {
   private createPostEndpoint = `${environment.apiUrl}/api/v1.0/calendar/post/`;
   private listPostsEndpoint = `${environment.apiUrl}/api/v1.0/calendar/post/?calendar_id=`;
 
-  constructor(private http: HttpClient, private  authService: AuthenticationService) {
+  constructor(private http: HttpClient) {
   }
 
   getPosts(calendarId: number) {
-    return this.http.get<Post[]>(this.listPostsEndpoint + calendarId.toString(), {
-      headers: new HttpHeaders({
-        Authorization: `Token ${this.authService.currentUserValue.key}`
-      })
-    });// todo give calendar id in the url
+    return this.http.get<Post[]>(this.listPostsEndpoint + calendarId.toString());
   }
 
   createPost(post) {
-    return this.http.post(this.createPostEndpoint, JSON.stringify((post)), {
-      headers: new HttpHeaders({
-        Authorization: `Token ${this.authService.currentUserValue.key}`
-      })
-    });
+    return this.http.post(this.createPostEndpoint, post);
   }
 }
