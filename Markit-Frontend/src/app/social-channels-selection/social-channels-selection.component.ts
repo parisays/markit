c5df6f783d838dfb2f1a2c88d52be3891e2aa710
YegmorLnx <yegmor@gmail.com>
2019-11-09 00:54:58 +0330

    Edit mistake in dictation

--- Markit-Frontend/src/app/social-channels-selection/social-channels-selection.component.ts
+++ Markit-Frontend/src/app/social-channels-selection/social-channels-selection.component.ts
@@ -8,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 export class SocialChannelsSelectionComponent implements OnInit {
 
   isTwitterSelected = false;
-  isPinterstSelected = false;
+  isPinterestSelected = false;
   isFacebookSelected = false;
   constructor() { }
 
