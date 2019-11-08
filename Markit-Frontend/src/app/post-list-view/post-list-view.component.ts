8c62c5ddb53581f538a7b8ed96d38eaa2b48c936
Mahnaz HaghaniNejad <mahnaz.hagh@outlook.com>
2019-11-08 19:48:04 +0330

    Add image to post list view

--- Markit-Frontend/src/app/post-list-view/post-list-view.component.ts
+++ Markit-Frontend/src/app/post-list-view/post-list-view.component.ts
@@ -98,7 +98,8 @@ export class PostListViewComponent implements OnInit {
       text: `Lithium is a chemical element with symbol Li and atomic number 3. It is a soft,
         silvery-white alkali metal. Under standard conditions, it is the lightest metal and the
         lightest solid element.`,
-      status: 'published'
+      status: 'published',
+      image: ''
     },
   ];
 
