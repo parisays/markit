ce3931f4321aa39b37ab2f1f6f18bee2a9b22111
YegmorLnx <yegmor@gmail.com>
2019-11-09 03:30:29 +0330

    Add post idea logic

--- Markit-Frontend/src/app/post-ideas/post-ideas.component.ts
+++ Markit-Frontend/src/app/post-ideas/post-ideas.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {PostIdeasService} from '@services';
+import {Calendar, PostIdea} from '@models';
 
 @Component({
   selector: 'app-post-ideas',
@@ -6,45 +8,56 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./post-ideas.component.scss']
 })
 export class PostIdeasComponent implements OnInit {
+  private postIdeas: PostIdea[] = null;
+  private loading = false;
 
   centered = false;
   disabled = false;
   unbounded = false;
-  constructor() { }
 
-  mockPostIdeas = [
-    {
-      type: 'twitter',
-      lable: '#test1',
-      text: 'kljckjbv;kj oskjdfoidjkjn dijfodjn ;oijf ii'
-    },
-    {
-      type: 'pinterest',
-      lable: '#test2',
-      text: 'kljckjbv;kj oskjdfoidjkjn dijfodjn ;oijf ii'
-    },
-    {
-      type: 'facebook',
-      lable: '#test3',
-      text: 'kljckjbv;kj oskjdfoidjkjn dijfodjn ;oijf ii'
-    },
-    {
-      type: 'twitter',
-      lable: '#test1',
-      text: 'kljckjbv;kj oskjdfoidjkjn dijfodjn ;oijf ii'
-    },
-    {
-      type: 'pinterest',
-      lable: '#test2',
-      text: 'kljckjbv;kj oskjdfoidjkjn dijfodjn ;oijf ii'
-    },
-    {
-      type: 'facebook',
-      lable: '#test3',
-      text: 'kljckjbv;kj oskjdfoidjkjn dijfodjn ;oijf ii'
-    }
-  ];
-  ngOnInit() {
+  constructor(private service: PostIdeasService) {
   }
+
+  ngOnInit() {
+    this.loading = true;
+
+    this.service.getAll().subscribe((response: PostIdea[]) => {
+      console.log(response);
+      this.postIdeas = response;
+      this.loading = false;
+    }, err => {
+      console.log(err);
+      this.loading = false;
+    });
+
+    // this.postIdeas = [
+    //   {
+    //     type: 'twitter',
+    //     lable: '#Flashback\n',
+    //     text: 'Post an image of the early days of your brand, or even funnier, of the founder of your brand as a child!'
+    //   },
+    //   {
+    //     type: ' pinterest',
+    //     lable: '#FeelGoodDay',
+    //     text: 'Post feel-good images or quotes related to your brand, industry or domain of expertise.'
+    //   },
+    //   {
+    //     type: 'facebook',
+    //     lable: 'Quote',
+    //     text: 'Everybody loves a good word: delight your audience with an inspiring or funny quote, expression or idiom.'
+    //   },
+    //   {
+    //     type: 'twitter',
+    //     lable: 'Miami Beach Pop Festival',
+    //     text: 'Miami Beach Pop Festival is proud to debut one of the most diverse, cross-genre, and collaborative music festival lineups in the world'
+    //   },
+    //   {
+    //     type: 'pinterest',
+    //     lable: 'National S.T.E.M Day\n',
+    //     text: 'Full S.T.E.A.M. ahead! November 8 is a day meant to inspire kids to explore and pursue their interests in Science, Technology, Engineering, Art and Math. '
+    //   }
+    // ];
+  }
+
 
 }
