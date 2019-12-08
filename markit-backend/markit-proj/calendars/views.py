716263d931aace515c611433ac40d1634bc2b451
Parisa Yalsavar <p.yalsavar@outlook.com>
2019-12-08 20:27:00 +0330

    Fix calendars views.py

--- markit-backend/markit-proj/calendars/views.py
+++ markit-backend/markit-proj/calendars/views.py
@@ -86,22 +86,6 @@ class CalendarView(generics.RetrieveUpdateDestroyAPIView):
         data.update(serializer.data)
         return Response(data)
 
-    # def get_object_or_404(queryset, *filter_args, **filter_kwargs):
-    #     """
-    #     Same as Django's standard shortcut, but make sure to also raise 404
-    #     if the filter_kwargs don't match the required types.
-    #     """
-    #     try:
-    #         return _get_object_or_404(queryset, *filter_args, **filter_kwargs)
-    #     except (TypeError, ValueError):
-    #         raise Http404
-
-    # def get_permissions(self):
-    #     """
-    #     Instantiates and returns the list of permissions that this view requires.
-    #     """
-    #     return [permission() for permission in self.permission_classes]
-
     def check_object_permissions(self, request, obj):
         """
         Check if the request should be permitted for a given object.
