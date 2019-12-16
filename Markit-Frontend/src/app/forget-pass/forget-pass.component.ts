0143452371b8e1e9423418ac5e80e0aca490e1b2
YegmorLnx <yegmor@gmail.com>
2019-12-16 13:55:21 +0330

    add logic for forget pass.

--- Markit-Frontend/src/app/forget-pass/forget-pass.component.ts
+++ Markit-Frontend/src/app/forget-pass/forget-pass.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {FormBuilder, Validators, FormGroup} from '@angular/forms';
 import {PasswordService} from '@services';
+import {MatSnackBar} from '@angular/material';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-forget-pass',
@@ -16,17 +18,26 @@ export class ForgetPassComponent implements OnInit {
     ]]
   });
 
-  constructor(private fb: FormBuilder, private service: PasswordService) {
+  constructor(private fb: FormBuilder,
+              private service: PasswordService,
+              private snackBar: MatSnackBar,
+              private router: Router) {
   }
 
   get email() {
-    return this.form.get('email');
+    return this.form.get('email').value;
   }
 
   ngOnInit() {
   }
 
   onSubmit() {
-    this.service.requestPasswordReset(this.email);
+    console.log(`email is:`, this.email);
+    this.service.requestPasswordReset({email: this.email}).subscribe(v => {
+      this.snackBar.open('Please Check your email!', 'OK', {duration: 3000});
+      this.router.navigate(['/']);
+    }, err => {
+      console.log(err);
+    });
   }
 }
