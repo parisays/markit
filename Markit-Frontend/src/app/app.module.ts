64fbb783ff7843c50f0765d652b78a5181dfee5f
Mahnaz HaghaniNejad <mahnaz.hagh@outlook.com>
2019-12-26 16:07:57 +0330

    Install ng zorro library

--- Markit-Frontend/src/app/app.module.ts
+++ Markit-Frontend/src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { ReactiveFormsModule } from '@angular/forms';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import { MdComponentsModule } from './md-components/md-components.module';
 import { MatSnackBarModule } from '@angular/material/snack-bar';
 import { SignupComponent } from './signup/signup.component';
@@ -35,6 +35,11 @@ import { ProfileComponent } from './profile/profile.component';
 import { TimepickerModule } from 'ngx-bootstrap/timepicker';
 import { ForgetPassComponent } from './forget-pass/forget-pass.component';
 import { ResetPassComponent } from './reset-pass/reset-pass.component';
+import { NgZorroAntdModule, NZ_I18N, en_US } from 'ng-zorro-antd';
+import { registerLocaleData } from '@angular/common';
+import en from '@angular/common/locales/en';
+
+registerLocaleData(en);
 
 @NgModule({
   declarations: [
@@ -78,9 +83,12 @@ import { ResetPassComponent } from './reset-pass/reset-pass.component';
     MatSnackBarModule,
     MatProgressSpinnerModule,
     TimepickerModule.forRoot(),
+    NgZorroAntdModule,
+    FormsModule,
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: AuthInterceptor, multi: true },
+    { provide: NZ_I18N, useValue: en_US }
   ],
   bootstrap: [AppComponent]
 })
