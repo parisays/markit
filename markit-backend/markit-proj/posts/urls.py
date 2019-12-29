6f103f01cc03f1fe1e56999322f258b7e4d538a4
Parisa Yalsavar <p.yalsavar@outlook.com>
2019-12-29 23:40:06 +0330

    Add user detail to collaborator response

--- markit-backend/markit-proj/posts/urls.py
+++ markit-backend/markit-proj/posts/urls.py
@@ -11,7 +11,7 @@ from .views import (
 
 urlpatterns = [
     url(r'^$', PostCreateView.as_view(), name='create-post'),
-    url(r'^(?P<calendar_id>\d+)$', PostListView.as_view(), name='list-calendar-posts'),
+    url(r'^(?P<calendar_id>\d+)/$', PostListView.as_view(), name='list-calendar-posts'),
     url(r'^edit/(?P<pk>\d+)/$', PostUpdateView.as_view(), name='edit-post'),
     url(r'^view/(?P<pk>\d+)/$', PostRetrieveView.as_view(), name='view-post'),
     url(r'^delete/(?P<pk>\d+)/$', PostDestroyView.as_view(), name='delete-post'),
