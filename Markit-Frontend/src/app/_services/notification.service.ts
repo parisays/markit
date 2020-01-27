b6435a550de241e2a912472b544b5032c3aff401
Mohammad Mahdi Abdollahpour <mohammad.m.a.77@outlook.com>
2020-01-27 13:30:21 +0330

    subscribe to ws

--- Markit-Frontend/src/app/_services/notification.service.ts
+++ Markit-Frontend/src/app/_services/notification.service.ts
@@ -3,6 +3,9 @@ import {webSocket} from 'rxjs/webSocket';
 import {Notification} from '@models';
 import {AuthenticationService} from '@app/_services/auth.service';
 import {from, Observable, of, BehaviorSubject} from 'rxjs';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '@environments/environment';
+import { filter } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -43,26 +46,32 @@ export class NotificationService {
 
   notifsSubject: BehaviorSubject<Notification[]>;
 
-  constructor(private authService: AuthenticationService) {
+  private seenEndpoint = `${environment.apiUrl}notification/seen/`;
+
+  constructor(private authService: AuthenticationService,
+              private http: HttpClient) {
     this.notifsSubject = new BehaviorSubject<Notification[]>(this.notifs);
 
-    const wsUrl = `ws://178.63.149.140:8000/ws/notification/${this.authService.currentUserValue.key}/`;
+    const wsUrl = `ws://194.5.193.99:8000/ws/notification/${this.authService.currentUserValue.key}/`;
     this.ws = webSocket(wsUrl);
-    // TODO subscribe ws
+    this.ws.pipe(
+      filter((d: Notification) => d.additionalData.notif_creator === this.authService.currentUserValue.email)
+    ).subscribe((d: Notification) => {
+      const nf = this.notifsSubject.getValue();
+      nf.push(d);
+      this.notifsSubject.next(nf);
+    });
   }
 
   getNotifications(): Observable<Notification[]> {
     return this.notifsSubject.asObservable();
   }
 
-  // getNotificationsCount(): Observable<number> {
-  //   return this.;
-  // }
-
   seenNotification(notifId: number): void {
-    // TODO req set seen
-    const nf = this.notifsSubject.getValue();
-    nf.splice(nf.findIndex(n => n.id === notifId), 1);
-    this.notifsSubject.next(nf);
+    this.http.post(`${this.seenEndpoint}${notifId}`, { }).subscribe(d => {
+      const nf = this.notifsSubject.getValue();
+      nf.splice(nf.findIndex(n => n.id === notifId), 1);
+      this.notifsSubject.next(nf);
+    });
   }
 }
