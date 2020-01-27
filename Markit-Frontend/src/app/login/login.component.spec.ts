df90bf8697b2b5f0ca6a87492271c4f1c913673b
YegmorLnx <yegmor@gmail.com>
2020-01-27 10:44:32 +0330

    Add test for login

--- Markit-Frontend/src/app/login/login.component.spec.ts
+++ Markit-Frontend/src/app/login/login.component.spec.ts
@@ -1,6 +1,15 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LoginComponent } from './login.component';
+import {LoginComponent} from './login.component';
+import {MatDividerModule, MatProgressSpinnerModule, MatSnackBar} from '@angular/material';
+import {MdComponentsModule} from '@app/md-components/md-components.module';
+import {BrowserModule} from '@angular/platform-browser';
+import {FormBuilder, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {ActivatedRoute, Router} from '@angular/router';
+import {AuthenticationService} from '@services';
+import {of} from 'rxjs';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
@@ -8,9 +17,40 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
+      imports: [MatDividerModule, MdComponentsModule, BrowserModule, FormsModule,
+        ReactiveFormsModule, MatProgressSpinnerModule, HttpClientModule, BrowserAnimationsModule],
+      declarations: [LoginComponent],
+      providers: [FormBuilder,
+        {
+          provide: Router,
+          useClass: class {
+            navigate = jasmine.createSpy('navigate');
+          }
+        },
+        AuthenticationService,
+        MatSnackBar,
+        {
+          provide: ActivatedRoute,
+          useValue: { // Mock
+            queryParams: of(
+              {
+                id_params: 'id_params_test'
+              }
+            ),
+            params: of(
+              {
+                id_query_params: 'id_query_params_test'
+              }
+            ),
+            snapshot: {
+              returnUrl: 'asdf',
+              queryParams: 'asdf'
+            }
+          }
+        }
+      ]
     })
-    .compileComponents();
+      .compileComponents();
   }));
 
   beforeEach(() => {
